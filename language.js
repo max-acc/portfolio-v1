48a35b3002c46b7b656a6745b7dc682fdf5cb5ed
An Alien <max.wenk@protonmail.com>
2023-07-15 10:11:20 +0200

    add content to development and add pictures

--- language.js
+++ language.js
@@ -5,31 +5,50 @@ var LanguageList = {
 
 //languages Objects
 var WORDS_EN = {
-  "header_home"     : "Home",
-  "header_skills"   : "Skills",
-  "header_projects" : "Projects",
-  "header_contact"  : "Contact",
+  "header_home"                 : "Home",
+  "header_skills"               : "Skills",
+  "header_projects"             : "Projects",
+  "header_contact"              : "Contact",
 
-  "section_backButton"  : "Go back!",
+  "section_backButton"          : "Go back!",
 
-  "section_home_header"       : "Home",
-  "section_home_hey"          : "Hey !",
-  "section_home_par1"         : "My name is Max and I really enjoy programming.",
-  "section_home_par2"         : "I am a computer scientist who is excitd to learn new stuff.",
-  "home_picture_description"  : "That's me!",
+  "section_home_header"         : "Home",
+  "section_home_hey"            : "Hey !",
+  "section_home_par1"           : "My name is Max and I really enjoy programming.",
+  "section_home_par2"           : "I am a computer scientist who is excitd to learn new stuff.",
+  "home_picture_description"    : "That's me!",
 
-  "section_aboutme_header" : "About me",
+  "section_aboutme_header"      : "About me",
 
-  "section_skills_header"   : "Skills",
+  "section_skills_header"       : "Skills",
+  "section_skills_development"  : "Development",
+  "section_skills_pythonCat"    : "Python",
+  "section_skills_pythonDes"    : "I'm develping in Python",
+  "section_skills_javaCat"      : "Java",
+  "section_skills_javaDes"      : "I'm developing in Java",
+  "section_skills_cCat"         : "C++/C#",
+  "section_skills_cDes"         : "I'm developing in C++ and C#",
+  "section_skills_webdevCat"    : "Web Development",
+  "section_skills_webdevDes"    : "as you might can see, i know some of the basics of web develpment in html css and javascript as this website is hardcoded. i know my design is not astetically pleasing but my written code is",
+  "section_skills_otherCat"     : "Other languages",
+  "section_skills_otherDes"     : "I know the basics of other languages",
+  "section_skills_otherDes"     : "I know the basics of other languages",
+  "section_skills_otherDes"     : "I know the basics of other languages",
 
-  "section_projects_header" : "Projects",
+  "section_skills_knowledge"    : "Knowledge",
+  "section_skills_mlCat"        : "Machine Learning",
+  "section_skills_mlDes"        : "I know the basics of other languages",
+  "section_skills_gitCat"       : "Git and GitHub",
+  "section_skills_gitDes"       : "I know the basics of other languages",
 
-  "section_contact_header"  : "Contact",
+  "section_projects_header"     : "Projects",
 
-  "footer_navigation"       : "Navigation",
-  "footer_gettouch"            : "Get in touch",
-  "footer_gettouch_github"     : "GitHub",
-  "footer_gettouch_linkedin"   : "LinkedIn"
+  "section_contact_header"      : "Contact",
+
+  "footer_navigation"           : "Navigation",
+  "footer_gettouch"             : "Get in touch",
+  "footer_gettouch_github"      : "GitHub",
+  "footer_gettouch_linkedin"    : "LinkedIn"
 };
 
 var WORDS_DE = {
