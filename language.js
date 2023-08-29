2b88318ae843e3c55e5093c8315c377ec1ed55ee
An Alien <max.wenk@protonmail.com>
2023-08-29 20:40:30 +0200

    add translation for skills

--- language.js
+++ language.js
@@ -55,31 +55,54 @@ var WORDS_EN = {
 };
 
 var WORDS_DE = {
-  "header_home"     : "Home",
-  "header_skills"   : "Fähigkeiten",
-  "header_projects" : "Projekte",
-  "header_contact"  : "Kontakt",
+  "header_home"                 : "Home",
+  "header_skills"               : "Fähigkeiten",
+  "header_projects"             : "Projekte",
+  "header_contact"              : "Kontakt",
 
-  "section_backButton"  : "Zurück zum Start",
+  "section_backButton"          : "Zurück zum Start",
 
-  "section_home_header"       : "Home",
-  "section_home_hey"          : "Hey !",
-  "section_home_par1"         : "Mein Name ist Max, und ich programmiere sehr gerne.",
-  "section_home_par2"         : "Ich bin ein Informatik-Mensch, der gerne neue Dinge lernt.",
-  "home_picture_description"  : "Das bin ich!",
+  "section_home_header"         : "Home",
+  "section_home_hey"            : "Hey !",
+  "section_home_par1"           : "Mein Name ist Max, und ich programmiere sehr gerne.",
+  "section_home_par2"           : "Ich bin ein Informatik-Mensch, der gerne neue Dinge lernt.",
+  "home_picture_description"    : "Das bin ich!",
 
-  "section_aboutme_header" : "Über mich",
+  "section_aboutme_header"      : "Über mich",
+  "aboutme_background"          : "Background",
+  "aboutme_description1"        : "I started my coding journey in 2016 when I was 12 years old. I gathered my first experiences during summer course for the Arduino Microcontroller at the student research center Phaenovum in Lörrach, Germany. Throughout the following years I continued developing basic knowledge in programming and computer science. A few years later, I discovered the web languages and was excited to learn them. During that time, I was limited to our families computer, which had time restrictions, so I was never able to fully immerse into programming. In late 2019, my school bought new computers, so they took some older computers out of service, which was in my favor because I was allowed to take some computers home. With the help of a friend, I upgraded the computer till it was usable by modern standards. That marks a caesura in my coding journey.",
+  "aboutme_description2"        : "After that I continued learning various languages and also improving my programming skills. Some languages I learned over the past few years are listed below in the Skills section. Over the past two years I developed an interest in Machine Learning, so I started my Machine Learning journey with another course at the Phaenovum. I just finished my first Machine learning project, and I am currently working on another project on image recoginition using special aspects for feature engineering.",
 
-  "section_skills_header"   : "Fähigkeiten",
+  "section_skills_header"       : "Fähigkeiten",
+  "section_skills_development"  : "Development",
+  "section_skills_pythonCat"    : "Python",
+  "section_skills_pythonDes"    : "In den letzten Jahren ist Python meine meist genutzte Programmiersprache geworden. Die Einfachheit und Schlichtheit beim schreiben von kurzen Skripten, aber auch die Klarheit beim Schreiben von größeren Programmen, macht die Sprache perfekt für micht. Die meisten meiner aktuellen Projekte wurden in Python programmiert.",
+  "section_skills_javaCat"      : "Java",
+  "section_skills_javaDes"      : "Obwohl Java nicht meine Lieblingssprache ist, benutze ich sie häufig, hauptsächlich auf Grund von Schulprojekten. Java wurde von meinen Lehrern verlangt, da alle Lernskripte und Projekte auf Java ausgelegt waren. In meinen späteren Schuljahren habe ich weiterhin Java benutzt, auf Grund der Einfachheit, Programme auf so gut wie jeden Gerät ohne Probleme ausführen zu können. Ein paar coole Prjekte die ich mit Java umgesetzt habe ist ein explizit programmierte Gesichtserkennung (ich weiß immer noch nicht, wie ich denken konnte das wäre eine gute Idee) und ich habe mein erstes Machine Learning Programm 'zu Fuß' in dieser Sprache programmiert, welches Blumen klassifizieren kann. Mein aktuellstes Projekt ist ein Neurales Netzwerk für Bilderkennung, welches das Feature-Engineering auf ein spezielle Art durchführt.",
+  "section_skills_cCat"         : "C++/C#",
+  "section_skills_cDes"         : "Wie bereits oben geschrieben, habe ich mit dem Programmieren durch Micorcontroller vertraut geworden, besonders über die Arduino Plattform. Da der Arduino normalerweise in C++ programmiert wird, kenne ich die Grundlagen in C++. Ich habe in den letzten Jahren einige Projekte programmiert, jedoch waren die meisten nur zum Spaß und sind nicht wirklich nennenswert. C++ hat mir geholfen die Grundlegenden Konzepte vom Programmieren, und später auch von Objekt-Orientierter-Programmierung, zu verstehen. Während der Covid Pandemie habe ich des Weiteren noch C# aus Spaß an der Sache gelernt.",
+  "section_skills_webdevCat"    : "Web Development",
+  "section_skills_webdevDes"    : "Wie man vielleicht sehen kann, behersche ich die Grundlagen in der Webprogrammierung in HTML5, CSS3 und JavaScript, da diese Website in diesen Sprachen programmiert ist. Ich weiß, das meine Designs visuell nicht an die Websites von UI oder UX Entwicklern dran kommt, aber je schlechter meine Designfähigkeiten sind, desto besser ist mein geschriebener Code. Zumindest hoffe ich das.",
+  "section_skills_otherCat"     : "Other languages",
+  "section_skills_otherDes"     : "Es gibt noch einige weitere Programmiersprachen, die ich in den letzten Jahren gelernt habe, wie zum Beispiel SQL, PHP oder R, jedoch bin ich mit diesen Sprachen nicht so vertraut, wie zum Beispiel mit Python. Jedoch bin ich der festen Überzeugung, das mir der Lernprozess geholfen hat, die grundlegenden Programmierkonzepte besser und tiefer zu verstehen, und es hat mir auch einen Ausblick auf andere Sprachen und ihre Nutzung gegeben.",
 
-  "section_projects_header" : "Projekte",
+  "section_skills_knowledge"    : "Kenntnisse",
+  "section_skills_mlCat"        : "Machine Learning",
+  "section_skills_mlDes"        : "Während eines Sommerkurses im Schülerforschungszentrum Phaenovum in Lörrach, bin ich das erste Mal auf Machine Learning aufmerksam geworden. Seitdem hat mich das Thema dann auch nicht mehr losgelassen. Besonders Bilderkennung bzw. Klassifikation interessiert mich besonders. Und seitdem entdecke ich das Themenfeld Machine Learning. Ein Jahr nach diesem Kurs habe ich dann angefangen mein erstes Machine Learning Programm zu programmieren. Das hört sich zunächst trivial an, jedoch habe ich das gesamte Programm selbst geschrieben, ohne mich auf anderen Programmiercode beziehen zu müssen. Dieser Prozess hat mir geholfen die Grundlagen von Machine Learning besser zu verstehen. Derzeit bin ich dabei ein tieferes Verständnis für Neuronale Netzwerke und ihre Programmierung zu erlangen. Mein Ziel ist eine Bilderkennung mit einer speziellen Art des Feature-Engineerings.",
+  "section_skills_gitCat"       : "Git and GitHub",
+  "section_skills_gitDes"       : "Ich weiß nicht mehr wie und wann, aber ich habe irgendwann Git als Versionskontrolle und GitHub als Datenbank zum Speichern dieser Versionen entdeckt. Seitdem habe ich es selten versäumt, diese Tools bei meinen Projekten zu verwenden. Meiner Meinung nach sind diese Tolls sehr sinnvoll und nützlich, besonders wenn man häufig neue Features in einem Projekt implementiert.",
 
-  "section_contact_header"  : "Kontakt",
 
-  "footer_navigation"       : "Navigation",
-  "footer_links"            : "In Kontakt treten",
-  "footer_links_github"     : "GitHub",
-  "footer_links_linkedin"   : "LinkedIn"
+  "section_projects_header"     : "Projekte",
+
+  "section_contact_header"      : "Kontakt",
+  "section_contact_interest"    : "Ich habe ihr Interesse geweckt?",
+  "section_contact_feel"        : "Kontaktieren Sie mich gerne!",
+
+  "footer_navigation"           : "Navigation",
+  "footer_links"                : "In Kontakt treten",
+  "footer_links_github"         : "GitHub",
+  "footer_links_linkedin"       : "LinkedIn"
 };
 
 
